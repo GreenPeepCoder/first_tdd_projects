1331edcbe8b5a596da05e8deea437bc57219a08c
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-06 14:34:45 -0500

    Skeleton of TDD Projects exercise

---
+++ lib/tdd_ex.rb
@@ -0,0 +1,11 @@
+def remove_dups(arr)
+end
+
+def two_sum(arr)
+end
+
+def my_transpose(rows)
+end
+
+def stock_picker(prices)
+end
---
+++ spec/tdd_ex_spec.rb
@@ -0,0 +1,16 @@
+require 'tdd_ex'
+require 'rspec'
+
+describe '#remove_dups' do
+end
+
+describe '#two_sum' do
+end
+
+describe '#my_transpose' do
+end
+
+describe '#stock_picker' do
+end
+
+
