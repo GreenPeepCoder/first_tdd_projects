ecee08a44c6b29344b7bf965e50ff181b3a7942f
williambrooksmiller <brooksmiller.miller@gmail.com>
2020-03-06 18:34:15 -0500

    Start implementation of specs for #my_uniq(array) method

--- spec/00_Array_spec.rb
+++ spec/00_Array_spec.rb
@@ -1,7 +1,20 @@
-require 'tdd_ex'
-require 'rspec'
+require '00_Array'
 
-describe '#my_uniq' do
+describe "my_uniq" do
+    let(:array) { [1, 3, 4, 1, 3, 7]}
+    let(:uniqued_array) { my_uniq(array.dup)}
+
+    it "removes duplicates" do
+        array.each do |item|
+            expect(uniqued_array.count(item)).to eq(1)
+        end
+    end
+
+    it "only contains items from the original array" do
+    end
+
+    it "does not modify original array" do
+    end
 end
 
 describe '#two_sum' do
