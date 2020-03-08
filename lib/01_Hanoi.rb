4aaa937545eed6a0da6099d80da09597f165c770
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-08 14:34:33 -0400

    Implement the specs and the code for the #move(from_tower, to_tower) method of the Hanoi game

--- lib/01_Hanoi.rb
+++ lib/01_Hanoi.rb
@@ -9,6 +9,22 @@ class TowersOfHanoiGame
         'Tower 1: ' + @stacks[1].join(' ') + "\n" +
         'Tower 2: ' + @stacks[2].join(' ') + "\n" 
     end
+
+    def valid_move?(from_tower, to_tower)
+        return false unless [from_tower, to_tower].all? { |i| i.between?(0, 2)}
+
+        !@stacks[from_tower].empty? && (
+            @stacks[to_tower].empty? ||
+            @stacks[to_tower].last > @stacks[from_tower].last
+        )
+    end
+
+    def move(from_tower, to_tower)
+        raise "cannot move from empty stack" if @stacks[from_tower].empty?
+        raise "cannot move onto smaller disc" unless valid_move?(from_tower, to_tower)
+        @stacks[to_tower] << @stacks[from_tower].pop
+    end
+
     private 
     attr_reader :stacks
 end
--- spec/01_hanoi_spec.rb
+++ spec/01_hanoi_spec.rb
@@ -11,7 +11,36 @@ describe TowersOfHanoiGame do
         it "prints shorter stacks"
     end 
 
-    describe "move"
+    describe "move" do
+        it "allows moving to a blank space" do
+            expect { towers.move(0,1)}.not_to raise_error
+        end
+        
+        it "allows moving onto a larger disc" do
+            towers.move(0,2)
+            towers.move(0,1)
+            expect { towers.move(2,1)}.not_to raise_error
+        end
+        
+        it "does not allow moving from an empty stack" do
+            expect { towers.move(1,2)}
+            .to raise_error("cannot move from empty stack")
+        end
+        
+        it "does not allow moving onto a smaller disc" do 
+            towers.move(0,1)
+            expect { towers.move(0, 1)}
+            .to raise_error("cannot move onto smaller disc")
+        end
+    end
 
-    describe "#won?"
+    describe "#won?" do
+        it "is not won at the start" do
+            expect(towers).not_to be_won
+        end
+
+        it "is won when all disks are moved to tower 1"
+
+        it "is won when all disks are moved to tower 2"
+    end
 end
