7c743c2f0ba696fa4a3b0c9ed79ccef76da872a4
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-03-08 14:13:23 -0400

    Set up specs for Hanoi.render, and implement the #render method

--- lib/01_Hanoi.rb
+++ lib/01_Hanoi.rb
@@ -1,4 +1,16 @@
 class TowersOfHanoiGame
+
+    def initialize
+        @stacks = [[3, 2, 1], [], []]
+    end
+
+    def render
+        'Tower 0: ' + @stacks[0].join(' ') + "\n" +
+        'Tower 1: ' + @stacks[1].join(' ') + "\n" +
+        'Tower 2: ' + @stacks[2].join(' ') + "\n" 
+    end
+    private 
+    attr_reader :stacks
 end
 
 if $PROGRAM_NAME == __FILE__
--- spec/01_hanoi_spec.rb
+++ spec/01_hanoi_spec.rb
@@ -1,4 +1,17 @@
 require '01_hanoi'
 
 describe TowersOfHanoiGame do
+    subject(:towers) { TowersOfHanoiGame.new }
+
+    describe "#render" do 
+        it "prints the stacks" do
+            expect(towers.render).to eq("Tower 0: 3 2 1\nTower 1: \nTower 2: \n")
+        end
+
+        it "prints shorter stacks"
+    end 
+
+    describe "move"
+
+    describe "#won?"
 end
